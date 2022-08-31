c33e32258c2641dd815148c1e9ea45c73067273c
Salvador Borit <5084@holbertonschool.com>
2022-08-31 19:27:50 -0300

    betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,11 +6,11 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (!tree)
-        return;
-    if (tree->left)
-        binary_tree_delete(tree->left);
-    if (tree->right)
-        binary_tree_delete(tree->right);
-    free(tree);
+	if (!tree)
+		return;
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
+	free(tree);
 }
