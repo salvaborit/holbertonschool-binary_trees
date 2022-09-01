8556cfc7acc4f033cd98d85f6f5089910dda804c
Salvador Borit <5084@holbertonschool.com>
2022-09-01 16:29:32 -0300

    attempt 1

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,15 +1,29 @@
 #include "binary_trees.h"
 
+
 /**
- *
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (NULL);
+		return (0);
 
-	while ()
-	{
+	return (my_binary_tree_height(tree));
+}
 
-	}
+size_t my_binary_tree_height(const binary_tree_t *tree)
+{
+	size_t leftHeight = 0, rightHeight = 0;
+
+	if (tree == NULL)
+		return (-1);
+
+	leftHeight = binary_tree_height(tree->left);
+	rightHeight = binary_tree_height(tree->right);
+
+	if (leftHeight > rightHeight)
+		return (leftHeight + 1);
+	return (rightHeight + 1);
 }
