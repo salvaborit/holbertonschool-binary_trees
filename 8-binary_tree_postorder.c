4070245b0904a3e71c60b00632e13d73987f87ba
Salvador Borit <5084@holbertonschool.com>
2022-08-31 22:11:34 -0300

    betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -2,7 +2,7 @@
 
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - goes through a binary tree using postorder traversal
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  */
