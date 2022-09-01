ac304492e542e5ee6c6ecad97da1962aa6d9b6db
Salvador Borit <5084@holbertonschool.com>
2022-09-01 16:56:33 -0300

    betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,6 +4,7 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -13,6 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (my_binary_tree_height(tree) - 1);
 }
 
+/**
+ * my_binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height
+ * Return: height of binary tree
+ */
 size_t my_binary_tree_height(const binary_tree_t *tree)
 {
 	size_t leftHeight = 0, rightHeight = 0;
