d4c4b984379a0105df231d507be9afc91a2449c3
Salvador Borit <5084@holbertonschool.com>
2022-09-02 22:21:44 -0300

    tabs

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,10 +7,10 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    return (binary_tree_sibling(node->parent));
+	return (binary_tree_sibling(node->parent));
 }
 
 /**
