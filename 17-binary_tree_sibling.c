f8b9d75c2cea3cae3ffb560673f8c2e458aaae72
Salvador Borit <5084@holbertonschool.com>
2022-09-02 21:48:24 -0300

    betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,16 +8,16 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node)
-        return (NULL);
+	if (!node || !node->parent)
+		return (NULL);
 
-    if (node->parent->left == node)
-        if (node->parent->right)
-            return (node->parent->right);
+	if (node->parent->left == node)
+		if (node->parent->right)
+			return (node->parent->right);
 
-    if (node->parent->right == node)
-        if (node->parent->left)
-                return (node->parent->left);
+	if (node->parent->right == node)
+		if (node->parent->left)
+				return (node->parent->left);
 
-    return (NULL);
+	return (NULL);
 }
